21cecf39515c37c96f4a9acc0bfc6eae9066176c
emilhomes <eb.milhomes@gmail.com>
2026-02-02 12:01:59 -0300

    auth-component done

--- auth-component/auth-component.ts
+++ auth-component/auth-component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-auth-component',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './auth-component.html',
   styleUrl: './auth-component.scss',
 })
 export class AuthComponent {
-
+  backgroundUrl = 'background-plants.png';
 }
